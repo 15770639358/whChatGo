9c333ee6d23407e3ef398ed92b1b495cdcdf1edb
zhang_1024 <2528450222@qq.com>
2020-12-24 16:56:35 +0800

    fix:增加组件

---
+++ miniprogram/interface/components/fileNavigation/fileNavigation.js
@@ -0,0 +1,27 @@
+// interface/components/fileNavigation/fileNavigation.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    //文件名
+    fileName:{
+      type:String,
+      value:""
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
